80a7524f196a82244324f644b6575d82a03560c2
liangkelei <69492036+liangkelei@users.noreply.github.com>
2022-04-26 19:43:45 -0400

    navbar to the top of the page

--- bujo/src/App.js
+++ bujo/src/App.js
@@ -10,22 +10,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <Navbar bg="light" expand="lg">
           <Container>
-            <Navbar.Brand href="#home">BuJo</Navbar.Brand>
+            <Navbar.Brand>BuJo</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#link">Text</Nav.Link>
+                <Nav.Link>Text</Nav.Link>
                 <NavDropdown title="Decorate" id="decorate-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Sticker</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Shape</NavDropdown.Item>
+                  <NavDropdown.Item>Sticker</NavDropdown.Item>
+                  <NavDropdown.Item>Shape</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Media" id="media-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Image</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Video</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">GIF</NavDropdown.Item>
+                  <NavDropdown.Item>Image</NavDropdown.Item>
+                  <NavDropdown.Item>Video</NavDropdown.Item>
+                  <NavDropdown.Item>GIF</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
