79056231a66fdfd9291e2e6a17959a2ba5e264a9
liangkelei <69492036+liangkelei@users.noreply.github.com>
2022-04-26 14:11:28 -0400

    added navbar

--- bujo/src/App.js
+++ bujo/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">BuJo</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#link">Text</Nav.Link>
+                <NavDropdown title="Decorate" id="decorate-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Sticker</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Shape</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Media" id="media-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Image</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Video</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">GIF</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
     </div>
   );
