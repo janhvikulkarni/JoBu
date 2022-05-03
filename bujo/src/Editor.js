07898c756e6e1d91edb2742fab234f28016890f5
Sophie Sun <ssun00@live.unc.edu>
2022-05-02 20:31:07 -0400

    attempted to change fonts

--- bujo/src/Editor.js
+++ bujo/src/Editor.js
@@ -3,7 +3,6 @@ import { EditorState } from 'draft-js';
 import { convertToHTML } from 'draft-convert';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { Editor } from 'react-draft-wysiwyg';
-import DOMPurify from 'dompurify';
 
 const EditorStyledToolbar = () => {
 
@@ -12,42 +11,49 @@ const EditorStyledToolbar = () => {
   );
   const  [convertedContent, setConvertedContent] = useState(null);
   const handleEditorChange = (state) => {
-    setEditorState(state);
+    setEditorState(state)
     convertContentToHTML();
   }
   const convertContentToHTML = () => {
     let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
     setConvertedContent(currentContentAsHTML);
   }
-  const createMarkup = (html) => {
-    return  {
-      __html: DOMPurify.sanitize(html)
-    }
-  }
+  
   return (
       <div style={{width: '400px'}} class="border border-primary">
         <Editor
           editorState={editorState}
           onEditorStateChange={handleEditorChange}
-          toolbarClassName="demo-toolbar-custom"
-          wrapperClassName="demo-wrapper"
-          editorClassName="demo-editor-custom"
+          wrapperClassName="wrapper-class"
+          editorClassName="editor-class"
+          toolbarClassName="toolbar-class"
           toolbar={{
             options: ['inline', 'blockType', 'fontSize', 'textAlign', 'fontFamily'],
             inline: {
-              options: ['bold', 'italic'],
+              // options: ['bold', 'italic'],
               bold: { className: 'demo-option-custom' },
               italic: { className: 'demo-option-custom' },
             },
-            blockType: { className: 'demo-option-custom-wide', dropdownClassName: 'demo-dropdown-custom' },
-            fontSize: { className: 'demo-option-custom-medium' },
+            blockType: { 
+              inDropdown: true,
+              options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote', 'Code'],
+              className: 'demo-option-custom-wide', 
+              dropdownClassName: 'demo-dropdown-custom' 
+            },
+            fontSize: { 
+              options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
+              className: 'demo-option-custom-medium' 
+            },
             textAlign: {
               options: ['left', 'center', 'right'],
               left: { className: 'demo-option-custom' },
               center: { className: 'demo-option-custom' },
               right: { className: 'demo-option-custom' },
             },
-            fontFamily: { className: 'demo-option-custom-wide', dropdownClassName: 'demo-dropdown-custom' },
+            fontFamily: { 
+              options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana'],
+              className: 'demo-option-custom-wide', dropdownClassName: 'demo-dropdown-custom' 
+          },
           }}
         />
       </div>
